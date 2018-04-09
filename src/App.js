616dc23bb76b3c9a5b29b00cf8d1c97bfd725ad7
Bastienrc <bastienrc@laposte.net>
2018-04-09 20:51:37 +0200

    Ajout d'un évenement onClick aux touches du clavier.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Keyboard, { DEFAULT_KEYBOARD } from './Keyboard';
+import Keyboard from './Keyboard';
+
+const keys = [
+  { id:  1, letter: "a", used: true },
+  { id:  2, letter: "b", used: true },
+  { id:  3, letter: "c", used: true },
+  { id:  4, letter: "d", used: true },
+  { id:  5, letter: "e", used: true },
+  { id:  6, letter: "f", used: true },
+  { id:  7, letter: "g", used: true },
+  { id:  8, letter: "h", used: true },
+  { id:  9, letter: "i", used: true },
+  { id: 10, letter: "j", used: true },
+  { id: 11, letter: "k", used: true },
+  { id: 12, letter: "l", used: false },
+  { id: 13, letter: "m", used: true },
+  { id: 14, letter: "n", used: true },
+  { id: 15, letter: "o", used: true },
+  { id: 16, letter: "p", used: false },
+  { id: 17, letter: "q", used: true },
+  { id: 18, letter: "r", used: true },
+  { id: 19, letter: "s", used: true },
+  { id: 20, letter: "t", used: false },
+  { id: 21, letter: "u", used: true },
+  { id: 22, letter: "v", used: true },
+  { id: 23, letter: "w", used: false },
+  { id: 24, letter: "x", used: true },
+  { id: 25, letter: "y", used: false },
+  { id: 26, letter: "z", used: false }
+];
 
 class App extends Component {
+  state = {
+    keys,
+    phrase: "constitutions",
+    usedLetters: new Set("a","r","j"),
+    score: 69
+  }
 
   // Produit une représentation textuelle de l’état de la partie,
   // chaque lettre non découverte étant représentée par un _underscore_.
@@ -11,12 +46,11 @@ class App extends Component {
     return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
   }
 
-  tabTest() {
-    return new Set(["a","e","t","o","n"]);
-  }
-
-  score() {
-    return 69;
+  handleKeyClick = letter => {
+    const { usedLetters } = this.state
+    usedLetters.add(letter)
+    this.setState({ usedLetters })
+    //return console.log(letter)
   }
 
   render() {
@@ -26,10 +60,10 @@ class App extends Component {
           <h1 className="App-title">Le Jeux du Pendu</h1>
         </header>
         <div className="App-screen">
-          <p className="App-score">Nombre de tentative : {this.score()}</p>
-          <p className="App-wordSearch">{this.computeDisplay("constitution", this.tabTest())}</p>
+          <p className="App-score">Nombre de tentative : {this.state.score}</p>
+          <p className="App-wordSearch">{this.computeDisplay(this.state.phrase, this.state.usedLetters)}</p>
         </div>
-        <Keyboard keys={DEFAULT_KEYBOARD} />
+        <Keyboard keys={this.state.keys} onClick={this.handleKeyClick}/>
       </div>
     );
   }
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -6,11 +6,11 @@ import React from 'react';
 import './Keyboard.css';
 import PropTypes from 'prop-types'
 
-const Keyboard = ({ keys }) => (
+const Keyboard = ({ keys, onClick }) => (
   <div className="keyboard">
     {
-      keys.map(({ id, letter, actived }) => (
-        <span key={id} className={actived ? "actived" : "desactived"}>{letter}</span>
+      keys.map(({ id, letter, used }) => (
+        <span key={id} className={used ? "key used" : "key"} onClick={() => onClick(letter)} >{letter}</span>
       ))
     }
   </div>
@@ -21,39 +21,9 @@ Keyboard.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       letter: PropTypes.string.isRequired,
-      actived: PropTypes.bool.isRequired
+      used: PropTypes.bool.isRequired
     })
   )
 }
 
 export default Keyboard;
-
-// Tableau par default
-export const DEFAULT_KEYBOARD = [
-  { id:  1, letter: "a", actived: true },
-  { id:  2, letter: "b", actived: true },
-  { id:  3, letter: "c", actived: false },
-  { id:  4, letter: "d", actived: true },
-  { id:  5, letter: "e", actived: true },
-  { id:  6, letter: "f", actived: true },
-  { id:  7, letter: "g", actived: true },
-  { id:  8, letter: "h", actived: true },
-  { id:  9, letter: "i", actived: true },
-  { id: 10, letter: "j", actived: true },
-  { id: 11, letter: "k", actived: true },
-  { id: 12, letter: "l", actived: false },
-  { id: 13, letter: "m", actived: true },
-  { id: 14, letter: "n", actived: true },
-  { id: 15, letter: "o", actived: true },
-  { id: 16, letter: "p", actived: false },
-  { id: 17, letter: "q", actived: true },
-  { id: 18, letter: "r", actived: true },
-  { id: 19, letter: "s", actived: true },
-  { id: 20, letter: "t", actived: false },
-  { id: 21, letter: "u", actived: true },
-  { id: 22, letter: "v", actived: true },
-  { id: 23, letter: "w", actived: false },
-  { id: 24, letter: "x", actived: true },
-  { id: 25, letter: "y", actived: false },
-  { id: 26, letter: "z", actived: false }
-];
