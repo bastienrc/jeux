7605068dd1495cd39ecd00a2c829c1618e403469
Bastienrc <bastienrc@laposte.net>
2018-10-25 09:36:41 +0200

    Correction de la syntaxe

--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-import Keyboard from './Keyboard';
+import Keyboard from './Keyboard'
 
-const phrases = ["pomme", "fraise", "orange", "banane", "clementine", "kiwi"];
+const phrases = ['pomme', 'fraise', 'orange', 'banane', 'clementine', 'kiwi']
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      keys: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+      keys: [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+      ],
       phrase: this.phraseRandom(phrases),
       usedLetters: new Set(),
-      score: 0
+      score: 0,
     }
   }
 
   phraseRandom(phrases) {
-    const min = Math.ceil(0);
-    const max = Math.floor(phrases.length-1);
-    const nbRandom = Math.floor(Math.random() * (max - min +1)) + min;
-    return phrases[nbRandom];
+    const min = Math.ceil(0)
+    const max = Math.floor(phrases.length - 1)
+    const nbRandom = Math.floor(Math.random() * (max - min + 1)) + min
+    return phrases[nbRandom]
   }
 
   // Produit une représentation textuelle de l’état de la partie,
   // chaque lettre non découverte étant représentée par un _underscore_.
   computeDisplay(phrase, usedLetters) {
-    return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'))
+    return phrase.replace(
+      /\w/g,
+      letter => (usedLetters.has(letter) ? letter : '_')
+    )
   }
 
   handleKeyClick = letter => {
@@ -52,13 +82,21 @@ class App extends Component {
           <h1 className="App-title">Le Jeu du Pendu</h1>
         </header>
         <div className="App-screen">
-          <p className="App-score">Nombre de tentative : {this.state.score}</p>
-          <p className="App-wordSearch">{this.computeDisplay(this.state.phrase, this.state.usedLetters)}</p>
+          <p className="App-score">
+            Nombre de tentative(s) : {this.state.score}
+          </p>
+          <p className="App-wordSearch">
+            {this.computeDisplay(this.state.phrase, this.state.usedLetters)}
+          </p>
         </div>
-        <Keyboard keys={this.state.keys} usedLetters={this.state.usedLetters} onClick={this.handleKeyClick}/>
+        <Keyboard
+          keys={this.state.keys}
+          usedLetters={this.state.usedLetters}
+          onClick={this.handleKeyClick}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,26 +2,26 @@
 // 26 lettres en deux ranger
 // Griser quand utiliser
 //
-import React from 'react';
-import './Keyboard.css';
+import React from 'react'
+import './Keyboard.css'
 import PropTypes from 'prop-types'
 
 const Keyboard = ({ keys, usedLetters, onClick }) => (
   <div className="keyboard">
-    {
-      keys.map(letter => (
-        <span
-          key={letter}
-          className={usedLetters.has(letter) ? "key used" : "key"}
-          onClick={() => onClick(letter)}
-        >{letter}</span>
-      ))
-    }
+    {keys.map(letter => (
+      <span
+        key={letter}
+        className={usedLetters.has(letter) ? 'key used' : 'key'}
+        onClick={() => onClick(letter)}
+      >
+        {letter}
+      </span>
+    ))}
   </div>
 )
 
 Keyboard.propTypes = {
-  keys: PropTypes.arrayOf(PropTypes.string.isRequired)
+  keys: PropTypes.arrayOf(PropTypes.string.isRequired),
 }
 
-export default Keyboard;
+export default Keyboard
