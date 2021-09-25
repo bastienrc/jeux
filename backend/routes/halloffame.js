a854f1112596cdc015fd036e7dfe1c43330bd67e
Sebastien Colbert <colbertsebastien@gmail.com>
2021-09-25 17:18:17 +0200

    Mise en place de l'authentification

--- backend/routes/halloffame.js
+++ backend/routes/halloffame.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const halloffameController = require('../controllers/halloffame')
+const auth = require('../middleware/auth')
 
-router.post('/', halloffameController.createScore)
-router.put('/:id', halloffameController.updateScore)
-router.delete('/:id', halloffameController.deleteScore)
+router.post('/', auth, halloffameController.createScore)
+router.put('/:id', auth, halloffameController.updateScore)
+router.delete('/:id', auth, halloffameController.deleteScore)
 router.get('/:id', halloffameController.readOneScore)
-router.get('/count', halloffameController.countScores)
+router.get('/count', auth, halloffameController.countScores)
 router.get('/', halloffameController.readAllScores)
 
 module.exports = router
